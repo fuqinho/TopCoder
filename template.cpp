c72ceda44aaff28644ac9a18bd48e7b6b0b4d995
fuqinho <kitakita7@gmail.com>
2013-03-29 03:41:02 +0900

    テンプレート修正。printfデバッグ用にSTLコンテナ用のoperator<<を追加したりした

---
+++ template.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <numeric>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cmath> 
+#include <ctime> 
+#include <cassert>
+using namespace std;
+
+#define REP(i,n) for(int i=0; i<(int)(n); i++)
+#define FOR(i,b,e) for(int i=(b); i<(int)(e); i++)
+#define EACH(i,c) for(__typeof((c).begin()) i=(c).begin(); i!=(c).end(); i++)
+#define ALL(c) (c).begin(), (c).end()
+$BEGINCUT$
+template<class T1,class T2> ostream& operator<<(ostream& o,const pair<T1,T2>& p){return o<<"("<<p.first<<","<<p.second<<")";}
+template<class T> ostream& operator<<(ostream& o,const vector<T>& v){o<<"[";for(typename vector<T>::const_iterator i=v.begin();i!=v.end();++i){if (i != v.begin()) o << ", ";o<<(*i);}o<<"]";return o;}
+template<class T> ostream& operator<<(ostream& o,const set<T>& s){o<<"{";for(typename set<T>::const_iterator i=s.begin();i!=s.end();++i){if(i!=s.begin())o<<", ";o<<(*i);}o<<"}";return o;}
+template<class K,class V> ostream& operator<<(ostream& o,const map<K,V>& m){o<<"{";for(typename map<K,V>::const_iterator i=m.begin();i!=m.end();++i){if(i!=m.begin())o<<", ";o<<i->first<<":"<<i->second;}o<<"}";return o;}
+string bitstr(int n,int d=0){string r;for(int i=0;i<d||n>0;++i,n>>=1){r+=(n&1)?"1":"0";}reverse(r.begin(),r.end());return r;}
+$ENDCUT$
+
+class $CLASSNAME$ {
+public:
+    $RC$ $METHODNAME$($METHODPARMS$) {
+    }
+};
+
+
+$BEGINCUT$
+$TESTCODE$ 
+
+$DEFAULTMAIN$
+$ENDCUT$

64f06ecd0e8a41c98007b5f9caee4ae183f60c5b
fuqinho <kitakita7@gmail.com>
2013-04-08 11:18:24 +0900

    updated template

--- template.cpp
+++ template.cpp
@@ -5,12 +5,13 @@
 #include <stack>
 #include <set>
 #include <map>
+#include <bitset>
 #include <algorithm>
 #include <numeric>
-#include <cstdio> 
-#include <cstdlib> 
-#include <cmath> 
-#include <ctime> 
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
 #include <cassert>
 using namespace std;
 
@@ -18,11 +19,14 @@ using namespace std;
 #define FOR(i,b,e) for(int i=(b); i<(int)(e); i++)
 #define EACH(i,c) for(__typeof((c).begin()) i=(c).begin(); i!=(c).end(); i++)
 #define ALL(c) (c).begin(), (c).end()
+#define dump(x) cerr << #x << " = " << (x) << endl;
+typedef long long ll;
 $BEGINCUT$
 template<class T1,class T2> ostream& operator<<(ostream& o,const pair<T1,T2>& p){return o<<"("<<p.first<<","<<p.second<<")";}
 template<class T> ostream& operator<<(ostream& o,const vector<T>& v){o<<"[";for(typename vector<T>::const_iterator i=v.begin();i!=v.end();++i){if (i != v.begin()) o << ", ";o<<(*i);}o<<"]";return o;}
 template<class T> ostream& operator<<(ostream& o,const set<T>& s){o<<"{";for(typename set<T>::const_iterator i=s.begin();i!=s.end();++i){if(i!=s.begin())o<<", ";o<<(*i);}o<<"}";return o;}
 template<class K,class V> ostream& operator<<(ostream& o,const map<K,V>& m){o<<"{";for(typename map<K,V>::const_iterator i=m.begin();i!=m.end();++i){if(i!=m.begin())o<<", ";o<<i->first<<":"<<i->second;}o<<"}";return o;}
+template<class T> ostream& operator<<(ostream& o,const vector<vector<T> >& m){o<<"[\n";for(typename vector<vector<T> >::const_iterator i=m.begin();i!=m.end();++i){o<<"  "<<(*i);o<<(i+1!=m.end()?",\n":"\n");}o<<"]\n";return o;}
 string bitstr(int n,int d=0){string r;for(int i=0;i<d||n>0;++i,n>>=1){r+=(n&1)?"1":"0";}reverse(r.begin(),r.end());return r;}
 $ENDCUT$
 
